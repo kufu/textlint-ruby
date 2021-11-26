89c21213084362f1739d5ca57e6a55cef99b1396
alpaca-tc <alpaca-tc@alpaca.tc>
2021-11-26 21:40:45 +0900

    Initialize repository

---
+++ lib/textlint/ruby/version.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module Textlint
+  module Ruby
+    VERSION = "0.1.0"
+  end
+end
---
+++ spec/textlint/ruby_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Textlint::Ruby do
+  it "has a version number" do
+    expect(Textlint::Ruby::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
